807eb56105e4ad64f01ed18ef0548eec038dcb9e
dgdfhfgdjhafsadf <josephzidapun0suy@hotmail.com>
2024-05-05 15:14:21 +0700

    feat project

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-function generate(numRows) {
-  const triangle = [];
-  for (let i = 0; i < numRows; i++) {
-    const row = new Array(i + 1).fill(1);
-    for (let j = 1; j < row.length - 1; j++) {
-      row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
+function isMatch(s, p) {
+  const dp = Array.from(Array(s.length + 1), () =>
+    Array(p.length + 1).fill(false),
+  );
+  dp[0][0] = true;
+  for (let i = 1; i <= p.length; i++) {
+    if (p[i - 1] === "*") {
+      dp[0][i] = dp[0][i - 2];
     }
-    triangle.push(row);
   }
-  return triangle;
+  for (let i = 1; i <= s.length; i++) {
+    for (let j = 1; j <= p.length; j++) {
+      if (s[i - 1] === p[j - 1] || p[j - 1] === ".") {
+        dp[i][j] = dp[i - 1][j - 1];
+      } else if (p[j - 1] === "*") {
+        dp[i][j] = dp[i][j - 2];
+        if (p[j - 2] === "." || s[i - 1] === p[j - 2]) {
+          dp[i][j] = dp[i][j] || dp[i - 1][j];
+        }
+      }
+    }
+  }
+  return dp[s.length][p.length];
 }
