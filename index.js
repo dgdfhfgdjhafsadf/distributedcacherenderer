6d5f9565f493eb13461a9a4f8abf4f3ee29b3be9
dgdfhfgdjhafsadf <josephzidapun0suy@hotmail.com>
2024-05-10 00:17:14 +0700

    update aptos project

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-function findTheDifference(s, t) {
-  let result = 0;
-  for (const char of s) {
-    result ^= char.charCodeAt(0);
-  }
-  for (const char of t) {
-    result ^= char.charCodeAt(0);
-  }
-  return String.fromCharCode(result);
-}
+const intersection = (a, b) => {
+  const s = new Set(b);
+  return a.filter((x) => s.has(x));
+};
